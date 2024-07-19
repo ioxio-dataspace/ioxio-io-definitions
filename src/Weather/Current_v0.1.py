ad760b8419f0da625b70de47812f6898a6a18684
Pekka Raittila <pekka@cocreators.ee>
2024-07-19 13:21:40 +0300

    Added units to field titles. Replaced rain with precipitation. Changed wording of descriptions to include units in plain text.

--- src/Weather/Current_v0.1.py
+++ src/Weather/Current_v0.1.py
@@ -5,16 +5,16 @@ from pydantic import Field
 class WeatherCurrentRequest(CamelCaseModel):
     latitude: float = Field(
         ...,
-        title="Latitude",
-        description="The latitude coordinate of the desired location",
+        title="Latitude (°)",
+        description="The latitude coordinate of the desired location in degrees",
         ge=-90.0,
         le=90.0,
         examples=[60.192059],
     )
     longitude: float = Field(
         ...,
-        title="Longitude",
-        description="The longitude coordinate of the desired location",
+        title="Longitude (°)",
+        description="The longitude coordinate of the desired location in degrees",
         ge=-180.0,
         le=180.0,
         examples=[24.945831],
@@ -24,46 +24,48 @@ class WeatherCurrentRequest(CamelCaseModel):
 class WeatherCurrentResponse(CamelCaseModel):
     temperature: float = Field(
         ...,
-        title="Temperature",
-        description="Current temperature in Celsius (°C)",
+        title="Temperature (°C)",
+        description="Current temperature in degrees Celsius",
         examples=[17.3],
         ge=-273.15,
     )
     humidity: float = Field(
         ...,
-        title="Humidity",
-        description="Current relative air humidity (%)",
+        title="Humidity (%)",
+        description="Current relative air humidity in percent",
         ge=0.0,
         le=100.0,
         examples=[72],
     )
     pressure: float = Field(
         ...,
-        title="Pressure",
-        description="Current air pressure (hPa)",
+        title="Pressure (hPa)",
+        description="Current air pressure in hectopascals",
         ge=0.0,
         examples=[1007],
     )
     wind_speed: float = Field(
         ...,
-        title="Wind speed",
-        description="Current wind speed (m/s)",
+        title="Wind speed (m/s)",
+        description="Current wind speed in meters per second",
         examples=[2.1],
         ge=0.0,
     )
     wind_direction: float = Field(
         ...,
-        title="Wind direction",
+        title="Wind direction (°)",
         description="Current wind direction in meteorological wind direction degrees",
         ge=0.0,
         le=360.0,
         examples=[220.0],
     )
-    rain: bool = Field(
+    precipitation: float = Field(
         ...,
-        title="Rain",
-        description="Is it currently raining?",
-        examples=[False],
+        title="Precipitation (mm/h)",
+        description="Current precipitation in millimeters per hour",
+        ge=0.0,
+        le=1000.0,
+        examples=[120.0],
     )
 
 
