ececcffe40e0b1912836b1ce650633edbdc76ebe
Pekka Raittila <pekka@cocreators.ee>
2024-07-19 14:25:27 +0300

    Changed the format of definition title, Changed CargoMoves to have properties of pick and place, changed HoistMoves to have properties lift and fall, made field descriptions clearer, updated ID title, added units to titles

--- src/CargoHandlingEquipment/OperationalPerformance_v0.1.py
+++ src/CargoHandlingEquipment/OperationalPerformance_v0.1.py
@@ -1,78 +1,68 @@
 import datetime
-from enum import Enum
-from typing import List, Optional
+from typing import Optional
 
 from definition_tooling.converter import CamelCaseModel, DataProductDefinition
 from pydantic import Field
 
 
-class CargoMoveType(str, Enum):
-    PICK = "pick"
-    PLACE = "place"
-
-
-class HoistMoveType(str, Enum):
-    LIFT = "lift"
-    FALL = "fall"
-
-
 class CargoMoves(CamelCaseModel):
-    type: Optional[CargoMoveType] = Field(
-        None,
-        title="Type",
-        description="The type of the cargo moves operated with the equipment",
-        examples=[CargoMoveType.PICK],
-    )
-    count: Optional[int] = Field(
-        None,
-        title="Count",
-        description="The number of cargo moves operated with the equipment",
+    pick: int = Field(
+        ...,
+        title="Number of pick moves",
+        description="Count of pick moves the cargo handling equipment has performed during the time period",
         examples=[52],
+    )
+
+    place: int = Field(
+        ...,
+        title="Number of place moves",
+        description="Count of place moves the cargo handling equipment has performed during the time period",
+        examples=[53],
     )
 
 
 class HoistMoves(CamelCaseModel):
-    type: Optional[HoistMoveType] = Field(
-        None,
-        title="Type",
-        description="The type of the hoist moves operated with the equipment",
-        examples=[HoistMoveType.LIFT],
-    )
-    count: Optional[int] = Field(
-        None,
-        title="Count",
-        description="The number of hoist moves operated with the equipment",
+    lift: int = Field(
+        ...,
+        title="Number of lift moves",
+        description="Count of lift moves the cargo handling equipment has performed during the time period",
         examples=[165],
+    )
+    fall: int = Field(
+        ...,
+        title="Number of fall moves",
+        description="Count of fall moves the cargo handling equipment has performed during the time period",
+        examples=[164],
     )
 
 
 class OperationalPerformanceResponse(CamelCaseModel):
     running_hours: float = Field(
         ...,
-        title="Running hours",
-        description="The total running hours (h)",
+        title="Running hours (h)",
+        description="The total running hours",
         examples=[550.0],
     )
     distance: float = Field(
         ...,
-        title="Cumulative distance",
-        description="The total distance driven in kilometers (km) ",
+        title="Cumulative distance (km)",
+        description="The total distance driven in kilometers",
         examples=[2500.0],
     )
-    cargo_moves: List[CargoMoves] = Field(
+    cargo_moves: CargoMoves = Field(
         ...,
         title="Cargo moves",
-        description="The details of the cargo handling moves",
+        description="Counts of cargo handling moves per type (pick or place)",
     )
-    hoist_moves: List[HoistMoves] = Field(
+    hoist_moves: HoistMoves = Field(
         ...,
         title="Hoist moves",
-        description="The details of the hoist moves",
+        description="Counts of hoist moves per type (lift or fall)",
     )
     cargo_weight_processed: Optional[float] = Field(
         None,
-        title="Cargo weight processed",
-        description="The total cargo weight processed kilograms (kg)",
+        title="Cargo weight processed (kg)",
+        description="The total cargo weight processed in kilograms",
         examples=[75.0],
     )
 
@@ -81,15 +71,14 @@ class OperationalPerformanceRequest(CamelCaseModel):
     id: str = Field(
         ...,
         max_length=40,
-        title="Id",
+        title="ID",
         description="The unique identifier of the product",
         examples=["71b51878-8a00-11ee-b9d1-0242ac120002"],
     )
     start_time: Optional[datetime.datetime] = Field(
         None,
         title="Start time",
-        description="The start time of the performance period in RFC 3339 format "
-        "following ISO 8601",
+        description="The start time of the performance period in RFC 3339 format",
         examples=[
             datetime.datetime(2023, 4, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
         ],
@@ -97,8 +86,7 @@ class OperationalPerformanceRequest(CamelCaseModel):
     end_time: Optional[datetime.datetime] = Field(
         None,
         title="End time",
-        description="The end time of the performance period in the desired country in "
-        "RFC 3339 format following ISO 8601",
+        description="The end time of the performance period in RFC 3339 format",
         examples=[
             datetime.datetime(2023, 5, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
         ],
@@ -107,7 +95,7 @@ class OperationalPerformanceRequest(CamelCaseModel):
 
 DEFINITION = DataProductDefinition(
     version="0.1.0",
-    title="Cargo Handling Equipment Operational Performance",
+    title="Cargo handling equipment operational performance",
     description="General operational status data of a mobile work machine operating in "
     "a port",
     request=OperationalPerformanceRequest,
