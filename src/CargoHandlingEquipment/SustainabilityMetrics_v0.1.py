dc3aee9fa37a51f6779f41a8bca1991f542c8c8c
Pekka Raittila <pekka@cocreators.ee>
2024-07-23 13:08:40 +0300

    Changed titles to hold units, removed extra mentions of standards and units, changed the casing of the name

--- src/CargoHandlingEquipment/SustainabilityMetrics_v0.1.py
+++ src/CargoHandlingEquipment/SustainabilityMetrics_v0.1.py
@@ -8,23 +8,23 @@ from pydantic import Field
 class SustainabilityResponse(CamelCaseModel):
     fuel_consumption: Optional[float] = Field(
         None,
-        title="Fuel consumption",
-        description="The total fuel consumption of the machine in litres (l)",
+        title="Fuel consumption (l)",
+        description="The total fuel consumption of the machine in litres",
         ge=0.0,
         examples=[1000.0],
     )
     gas_consumption: Optional[float] = Field(
         None,
-        title="Gas consumption",
-        description="The total gas consumption of the equipment in kilograms (kg)",
+        title="Gas consumption (kg)",
+        description="The total gas consumption of the equipment in kilograms",
         ge=0.0,
         examples=[3000.0],
     )
     electricity_consumption: Optional[float] = Field(
         None,
-        title="Electricity consumption",
+        title="Electricity consumption (kWh)",
         description="The total cumulative electricity consumption of the equipment in "
-        "kilowatt hours (kWh)",
+        "kilowatt hours",
         ge=0.0,
         examples=[1000.0],
     )
@@ -34,15 +34,14 @@ class SustainabilityRequest(CamelCaseModel):
     id: str = Field(
         ...,
         max_length=40,
-        title="Id",
+        title="ID",
         description="The unique identifier of the product",
         examples=["71b51878-8a00-11ee-b9d1-0242ac120002"],
     )
     start_time: datetime.datetime = Field(
         ...,
         title="Start time",
-        description="The start time of the metric period in RFC 3339 format following "
-        "ISO 8601",
+        description="The start time of the metric period in RFC 3339 format",
         examples=[
             datetime.datetime(2023, 4, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
         ],
@@ -50,8 +49,7 @@ class SustainabilityRequest(CamelCaseModel):
     end_time: datetime.datetime = Field(
         ...,
         title="End time",
-        description="The end time of the metric period in RFC 3339 format following "
-        "ISO 8601",
+        description="The end time of the metric period in RFC 3339 format",
         examples=[
             datetime.datetime(2023, 5, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
         ],
@@ -60,7 +58,7 @@ class SustainabilityRequest(CamelCaseModel):
 
 DEFINITION = DataProductDefinition(
     version="0.1.0",
-    title="Cargo Handling Equipment Sustainability Metrics",
+    title="Cargo handling equipment sustainability metrics",
     description="The power source consumption for the sustainability evaluation of the "
     "cargo handling equipment operations",
     request=SustainabilityRequest,
