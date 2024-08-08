f7eab42267565131da245f07d0ff4b94059c0a82
Pekka Raittila <pekka@cocreators.ee>
2024-08-08 10:17:15 +0300

    Made cargomoves and hoist moves optional Added start time and end time to response as well Changed description from fall to lower

--- src/CargoHandlingEquipment/OperationalPerformance_v0.1.py
+++ src/CargoHandlingEquipment/OperationalPerformance_v0.1.py
@@ -6,15 +6,15 @@ from pydantic import Field
 
 
 class CargoMoves(CamelCaseModel):
-    pick: int = Field(
-        ...,
+    pick: Optional[int] = Field(
+        None,
         title="Number of pick moves",
         description="Count of pick moves the cargo handling equipment has performed during the time period",
         examples=[52],
     )
 
-    place: int = Field(
-        ...,
+    place: Optional[int] = Field(
+        None,
         title="Number of place moves",
         description="Count of place moves the cargo handling equipment has performed during the time period",
         examples=[53],
@@ -22,14 +22,14 @@ class CargoMoves(CamelCaseModel):
 
 
 class HoistMoves(CamelCaseModel):
-    lift: int = Field(
-        ...,
+    lift: Optional[int] = Field(
+        None,
         title="Number of lift moves",
         description="Count of lift moves the cargo handling equipment has performed during the time period",
         examples=[165],
     )
-    lower: int = Field(
-        ...,
+    lower: Optional[int] = Field(
+        None,
         title="Number of lowering moves",
         description="Count of lowering moves the cargo handling equipment has performed during the time period",
         examples=[164],
@@ -37,27 +37,43 @@ class HoistMoves(CamelCaseModel):
 
 
 class OperationalPerformanceResponse(CamelCaseModel):
+    start_time: Optional[datetime.datetime] = Field(
+        None,
+        title="Start time",
+        description="The start time of the performance period in RFC 3339 format",
+        examples=[
+            datetime.datetime(2023, 4, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
+        ],
+    )
+    end_time: Optional[datetime.datetime] = Field(
+        None,
+        title="End time",
+        description="The end time of the performance period in RFC 3339 format",
+        examples=[
+            datetime.datetime(2023, 5, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
+        ],
+    )
     running_hours: float = Field(
         ...,
         title="Running hours (h)",
-        description="The total running hours",
+        description="The running hours during the specified time range",
         examples=[550.0],
     )
     distance: float = Field(
         ...,
-        title="Cumulative distance (km)",
-        description="The total distance driven in kilometers",
+        title="Distance (km)",
+        description="The total distance driven in kilometers during the specified time range",
         examples=[2500.0],
     )
-    cargo_moves: CargoMoves = Field(
-        ...,
+    cargo_moves: Optional[CargoMoves] = Field(
+        None,
         title="Cargo moves",
         description="Counts of cargo handling moves per type (pick or place)",
     )
-    hoist_moves: HoistMoves = Field(
-        ...,
+    hoist_moves: Optional[HoistMoves] = Field(
+        None,
         title="Hoist moves",
-        description="Counts of hoist moves per type (lift or fall)",
+        description="Counts of hoist moves per type (lift or lower)",
     )
     cargo_weight_processed: Optional[float] = Field(
         None,
