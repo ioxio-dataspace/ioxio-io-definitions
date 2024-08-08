8d9ce4eac0c6be7d0bee99d1ed4b7abeb12cf7a6
Pekka Raittila <pekka@cocreators.ee>
2024-08-08 10:48:17 +0300

    removed RFC 3339 form titles changed field names to snake_case fixed typos in descriptions changed twist locks closed description fixed validation for heading changed spreader lenght description added validation for sample interval changed typo in title

--- src/CargoHandlingEquipment/SimulationMetrics_v0.1.py
+++ src/CargoHandlingEquipment/SimulationMetrics_v0.1.py
@@ -27,7 +27,7 @@ class LocationCoordinates(CamelCaseModel):
 class Sample(CamelCaseModel):
     time: datetime.datetime = Field(
         ...,
-        title="Time (RFC 3339)",
+        title="Time",
         description="The time of the equipment's performance sample in RFC 3339 format",
         examples=[
             datetime.datetime(2023, 4, 12, 23, 20, 50, tzinfo=datetime.timezone.utc),
@@ -46,26 +46,26 @@ class Sample(CamelCaseModel):
         description="The instantaneous load of the equipment in kilograms",
         examples=[5000],
     )
-    loadHeight: float = Field(
+    load_height: float = Field(
         ...,
         title="Load height (mm)",
         description="The instantaneous load height in millimeters from the ground",
         examples=[2000],
     )
-    hoistSpeed: float = Field(
+    hoist_speed: float = Field(
         ...,
         title="Hoist speed (mm/s)",
-        description="The instantanous speed of the equipment hoist move in millmeters per second from the ground",
+        description="The instantaneous speed of the equipment hoist move in millmeters per second from the ground",
         examples=[500],
     )
-    twistLocksClosed: bool = Field(
+    twist_locks_closed: bool = Field(
         ...,
         title="Twist locks closed",
-        description="When twist locks are closed, the container can be hoisted",
+        description="Whether twist locks are closed or not. When they are closed, the container can be hoisted.",
         examples=["True"],
     )
 
-    location: LocationCoordinates = Field(
+    locationCoordinates: LocationCoordinates = Field(
         ...,
         title="Location",
         description="The location of the equipment in GPS coordinates",
@@ -75,37 +75,37 @@ class Sample(CamelCaseModel):
         title="Heading (°)",
         description="The GPS heading of the equipment measured from the true north in degrees",
         ge=0.0,
-        le=359.0,
+        le=360.0,
         examples=["90.5"],
     )
-    drivePower: Optional[float] = Field(
+    drive_power: Optional[float] = Field(
         None,
         title="Drive power (kW)",
         description="The instantaneous power of the equipment for driving on the ground in kilowatts",
     )
-    hoistPower: Optional[float] = Field(
+    hoist_power: Optional[float] = Field(
         None,
         title="Hoist power (kW)",
         description="The instantaneous power of the equipment for hoisting load in kilowatts",
     )
-    auxiliaryPower: Optional[float] = Field(
+    auxiliary_power: Optional[float] = Field(
         None,
         title="Auxiliary power (kW)",
-        description="Other energy consumption in kilowatts",
+        description="The instantaneous auxiliary (other) energy consumption in kilowatts",
     )
-    spreaderLenght: Optional[float] = Field(
+    spreader_length: Optional[float] = Field(
         None,
         title="Spreader length (ft)",
-        description="The instantaneous length of the equipment spreader measured in feet",
+        description="The length of the equipment spreader measured in feet",
         examples=["20"],
     )
-    idleStatus: bool = Field(
+    idle_status: bool = Field(
         ...,
         title="Idle status",
         description="The instantaneous idling status of the equipment. Idling means the equipment is not receiving any command# from the driver (joystick) and it is not moving any element to perform a job (drive, hoist, gantry, trolley, etc)",
         examples=["True", "False"],
     )
-    drivingForward: bool = Field(
+    driving_forward: bool = Field(
         ...,
         title="Driving forward",
         description="The instantaneous driving status of the equipment",
@@ -117,7 +117,7 @@ class Sample(CamelCaseModel):
         description="The instantaneous acceleration of the equipment on the ground in millimeters per second per second",
         examples=["1000"],
     )
-    hoistAccelerator: Optional[float] = Field(
+    hoist_accelerator: Optional[float] = Field(
         None,
         title="Hoist acceleration (mm/s^2)",
         description="The instantaneous acceleration for hoisting the load in millimeters per second per second per second",
@@ -125,13 +125,14 @@ class Sample(CamelCaseModel):
 
 
 class SimulationMetricsResponse(CamelCaseModel):
-    sampleCount: int = Field(
+    sample_count: int = Field(
         ...,
         title="Sample count",
         description="The number of samples received in the response",
         examples=["10000"],
+        ge=0,
     )
-    samples: Sample = Field(
+    samples: list[Sample] = Field(
         ..., title="Samples", description="List of equipment's performance samples"
     )
 
@@ -142,23 +143,18 @@ class SimulationMetricsRequest(CamelCaseModel):
         title="ID",
         description="The unique identifier of the data source",
     )
-    startTime: datetime.datetime = Field(
+    start_time: datetime.datetime = Field(
         ...,
-        title="Start time (RFC 3339)",
-        description="The start time of the sample period in RFC 3339 format.",
+        title="Start time",
+        description="The start time of the sample period in RFC 3339 format",
         examples=[
             datetime.datetime(
                 2023, 4, 12, 23, 20, 50, 52, tzinfo=datetime.timezone.utc
             ),
         ],
     )
-    sampleInterval: int = Field(
-        title="Sample interval (ms)",
-        description="The requested sample interval smaller or equal to 1000 milliseconds",
-        example=["500"],
-    )
-    endTime: datetime.datetime = Field(
-        ...,
+    end_time: Optional[datetime.datetime] = Field(
+        None,
         title="End time (RFC 3339)",
         description="The end time of the sample period in RFC 3339 format",
         examples=[
@@ -167,11 +163,18 @@ class SimulationMetricsRequest(CamelCaseModel):
             ),
         ],
     )
+    sample_interval: int = Field(
+        title="Sample interval (ms)",
+        description="The requested sample interval smaller or equal to 1000 milliseconds",
+        example=["500"],
+        ge=0,
+        le=1000,
+    )
 
 
 DEFINITION = DataProductDefinition(
     version="0.1.0",
-    title="Cargo handling equipment Simulation metrics",
+    title="Cargo handling equipment simulation metrics",
     description="General simulation metrics of a cargo handling equipment operating in a port",
     request=SimulationMetricsRequest,
     response=SimulationMetricsResponse,
