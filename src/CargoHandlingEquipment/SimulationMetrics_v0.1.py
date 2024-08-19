69978a63e585be461b4070da3e84a703e4b0c155
Joakim Nordling <joakim.nordling@ioxio.com>
2024-08-19 11:01:06 +0300

    Minor fixes

--- src/CargoHandlingEquipment/SimulationMetrics_v0.1.py
+++ src/CargoHandlingEquipment/SimulationMetrics_v0.1.py
@@ -55,17 +55,17 @@ class Sample(CamelCaseModel):
     hoist_speed: float = Field(
         ...,
         title="Hoist speed (mm/s)",
-        description="The instantaneous speed of the equipment hoist move in millmeters per second from the ground",
+        description="The instantaneous speed of the equipment hoist move in millimeters per second from the ground",
         examples=[500],
     )
     twist_locks_closed: bool = Field(
         ...,
         title="Twist locks closed",
         description="Whether twist locks are closed or not. When they are closed, the container can be hoisted.",
-        examples=["True"],
+        examples=[True],
     )
 
-    locationCoordinates: LocationCoordinates = Field(
+    location_coordinates: LocationCoordinates = Field(
         ...,
         title="Location",
         description="The location of the equipment in GPS coordinates",
@@ -76,7 +76,7 @@ class Sample(CamelCaseModel):
         description="The GPS heading of the equipment measured from the true north in degrees",
         ge=0.0,
         le=360.0,
-        examples=["90.5"],
+        examples=[90.5],
     )
     drive_power: Optional[float] = Field(
         None,
@@ -97,30 +97,31 @@ class Sample(CamelCaseModel):
         None,
         title="Spreader length (ft)",
         description="The length of the equipment spreader measured in feet",
-        examples=["20"],
+        examples=[20.0],
     )
-    idle_status: bool = Field(
-        ...,
+    idle_status: Optional[bool] = Field(
+        None,
         title="Idle status",
         description="The instantaneous idling status of the equipment. Idling means the equipment is not receiving any command from the driver (joystick) and it is not moving any element to perform a job (drive, hoist, gantry, trolley, etc).",
-        examples=["True", "False"],
+        examples=[True],
     )
-    driving_forward: bool = Field(
-        ...,
+    driving_forward: Optional[bool] = Field(
+        None,
         title="Driving forward",
         description="The instantaneous driving status of the equipment",
-        examples=["True", "False"],
+        examples=[True],
     )
     acceleration: Optional[float] = Field(
         None,
         title="Acceleration (mm/s^2)",
         description="The instantaneous acceleration of the equipment on the ground in millimeters per second per second",
-        examples=["1000"],
+        examples=[1000.0],
     )
     hoist_accelerator: Optional[float] = Field(
         None,
         title="Hoist acceleration (mm/s^2)",
-        description="The instantaneous acceleration for hoisting the load in millimeters per second per second per second",
+        description="The instantaneous acceleration for hoisting the load in millimeters per second per second",
+        examples=[500.0],
     )
 
 
@@ -129,11 +130,13 @@ class SimulationMetricsResponse(CamelCaseModel):
         ...,
         title="Sample count",
         description="The number of samples received in the response",
-        examples=["10000"],
+        examples=[10000],
         ge=0,
     )
     samples: list[Sample] = Field(
-        ..., title="Samples", description="List of equipment's performance samples"
+        ...,
+        title="Samples",
+        description="List of equipment's performance samples",
     )
 
 
@@ -159,14 +162,15 @@ class SimulationMetricsRequest(CamelCaseModel):
         description="The end time of the sample period, in RFC 3339 format",
         examples=[
             datetime.datetime(
-                2023, 4, 12, 23, 20, 50, 52, tzinfo=datetime.timezone.utc
+                2023, 5, 12, 23, 20, 50, 52, tzinfo=datetime.timezone.utc
             ),
         ],
     )
-    sample_interval: int = Field(
+    sample_interval: Optional[int] = Field(
+        None,
         title="Sample interval (ms)",
         description="The requested sample interval smaller or equal to 1000 milliseconds",
-        example=["500"],
+        example=[500],
         ge=0,
         le=1000,
     )
