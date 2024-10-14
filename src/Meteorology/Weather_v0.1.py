70096a658e226ddbe75b30b32d375ad5457238f7
fbjorn <denis@fbjorn.cc>
2024-10-14 16:54:48 +0300

    Modify descriptions

--- src/Meteorology/Weather_v0.1.py
+++ src/Meteorology/Weather_v0.1.py
@@ -9,7 +9,7 @@ class WeatherRequest(CamelCaseModel):
     lat: float = Field(
         ...,
         title="Latitude (°)",
-        description="The latitude coordinate of the desired location in degrees",
+        description="The latitude coordinate of the desired location in degrees.",
         ge=-90.0,
         le=90.0,
         examples=[60.192059],
@@ -17,7 +17,7 @@ class WeatherRequest(CamelCaseModel):
     lon: float = Field(
         ...,
         title="Longitude (°)",
-        description="The longitude coordinate of the desired location in degrees",
+        description="The longitude coordinate of the desired location in degrees.",
         ge=-180.0,
         le=180.0,
         examples=[24.945831],
@@ -25,7 +25,7 @@ class WeatherRequest(CamelCaseModel):
     when: Optional[datetime.datetime] = Field(
         None,
         title="Date and time",
-        description="Date and time in RFC 3339 format. If not set, request the current weather",
+        description="Date and time in RFC 3339 format. If not specified, returns the current weather.",
         examples=[
             datetime.datetime(2023, 4, 12, 23, 20, 50, tzinfo=datetime.timezone.utc)
         ],
@@ -36,14 +36,14 @@ class WeatherResponse(CamelCaseModel):
     temperature: float = Field(
         ...,
         title="Temperature (°C)",
-        description="Current temperature in degrees Celsius",
+        description="Current temperature in degrees Celsius.",
         examples=[17.3],
         ge=-273.15,
     )
     humidity: Optional[float] = Field(
         None,
         title="Humidity (%)",
-        description="Current relative air humidity percentage",
+        description="Current relative air humidity percentage.",
         ge=0.0,
         le=100.0,
         examples=[72],
@@ -51,21 +51,21 @@ class WeatherResponse(CamelCaseModel):
     pressure: Optional[float] = Field(
         None,
         title="Pressure (hPa)",
-        description="Current air pressure in hectopascals",
+        description="Current air pressure in hectopascals.",
         ge=0.0,
         examples=[1007],
     )
     wind_speed: Optional[float] = Field(
         None,
         title="Wind speed (m/s)",
-        description="Current wind speed in meters per second",
+        description="Current wind speed in meters per second.",
         examples=[2.1],
         ge=0.0,
     )
     wind_direction: Optional[float] = Field(
         None,
         title="Wind direction (°)",
-        description="Current wind direction in meteorological wind direction degrees",
+        description="Current wind direction in meteorological wind direction degrees.",
         ge=0.0,
         le=360.0,
         examples=[220.0],
@@ -73,7 +73,7 @@ class WeatherResponse(CamelCaseModel):
     precipitation: Optional[float] = Field(
         None,
         title="Precipitation (mm/h)",
-        description="Current precipitation in millimeters per hour",
+        description="Current precipitation in millimeters per hour.",
         ge=0.0,
         le=1000.0,
         examples=[120.0],
@@ -81,7 +81,7 @@ class WeatherResponse(CamelCaseModel):
     visibility: Optional[float] = Field(
         None,
         title="Visibility (m)",
-        description="Visibility in meters",
+        description="Visibility in meters.",
         ge=0.0,
         le=300000.0,
         examples=[320.0],
